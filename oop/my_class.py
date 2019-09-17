46739726890448c8169f38bafeb38d8bd52ec5ea
jwech <joewei0208@gmail.com>
2019-09-17 20:13:12 +0800

    add user and dog class

--- oop/my_class.py
+++ oop/my_class.py
@@ -1,3 +1,26 @@
+# Create Dog Class
+class Dog:
+    '''Simulate dog'''
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        '''Simulate when ordered to sit'''
+        print(self.name.title() + ' is now sitting.')
+    
+    def roll_over(self):
+        '''Simulate when ordered to roll over'''
+        print(self.name.title() + " rolled over.")
+
+my_dog = Dog('willie', 6)
+# access attributes with .
+print("My dog's name is " + my_dog.name.title()) 
+print("My dog is " + str(my_dog.age) + " years old.")
+# call methods
+my_dog.sit()
+my_dog.roll_over()
+
 class Restaurant:
     '''Simulate Restaurant'''
     def __init__(self, restaurant_name, cuisine_type):
@@ -5,7 +28,7 @@ class Restaurant:
         self.type = cuisine_type
 
     def describe_restaurant(self):
-        print('Name:', self.Name)
+        print('Name:', self.name)
         print('Type:', self.type)
 
     def open_restaurant(self):
@@ -14,3 +37,22 @@ class Restaurant:
 r = Restaurant('pizza', 'snack')
 r.describe_restaurant()
 r.open_restaurant()
+
+class User:
+    '''User class includes infos of users'''
+    def __init__(self, first_name, last_name):
+        self.first = first_name
+        self.last = last_name
+    
+    def describe_user(self):
+        '''print infos of user'''
+        print('First name:', self.first.title())
+        print('Last name:', self.last.title())
+    
+    def greet_user(self):
+        '''greet user'''
+        print('Hello,', self.first.title(), self.last.title())
+
+u = User('joe', 'wei')
+u.describe_user()
+u.greet_user()
