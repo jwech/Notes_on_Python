8fe4c54eb900058d3eb0f492db928c9c476bb81c
jwech <joewei0208@gmail.com>
2019-09-18 17:23:08 +0800

    edit my_exceptions.py

--- oop/my_exceptions.py
+++ oop/my_exceptions.py
@@ -30,5 +30,28 @@ try:
     with open(filename) as f:
         contents = f.read()
 except FileNotFoundError:
-    msg = "Sorry, the file " + filename + "does not exist."
+    msg = "Sorry, the file " + filename + " does not exist."
     print(msg)
+else:
+    words = contents.split()
+    num_words = len(words)
+    print('The file ' + filename + ' has about ' + str(num_words) + ' words.')
+
+# Access multiple files
+def count_words(filename):
+    # Calculate how many words in a file
+    try:
+        with open(filename) as f:
+            contents = f.read()
+    except FileNotFoundError:
+        msg = "Sorry, the file " + filename + " does not exist."
+        print(msg)
+    else:
+        # Calculate numbers of words
+        words = contents.split()
+        num_words = len(words)
+        print("The file " + filename + " has about " + str(num_words) + " words.")
+
+filename = 'programming.txt'
+count_words(filename)
+
