72ed6e77045f1e0f758155271d308fcad7625db2
jwech <joewei0208@gmail.com>
2019-09-18 08:28:32 +0800

    edit my_files.py

--- oop/my_files.py
+++ oop/my_files.py
@@ -1,3 +1,23 @@
-with open('./oop/pi_digits.txt') as f:
+filename = './oop/pi_digits.txt'
+
+# Read all lines
+with open(filename) as f:
     contents = f.read()
-    print(contents)
+    print(contents.rstrip())
+
+# Read line by line
+with open(filename) as f:
+    for line in f:
+        print(line.rstrip())
+
+# Create list contains all lines
+with open(filename) as f:
+    lines = f.readlines()
+print(lines)
+
+# Access contents in file
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+print(pi_string)
+print(len(pi_string))
