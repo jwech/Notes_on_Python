e7b93d7d4920c50ef2599f6d638e84ee364850bf
jwech <joewei0208@gmail.com>
2019-09-19 10:09:49 +0800

    edit my_exceptinos.py

--- oop/my_exceptions.py
+++ oop/my_exceptions.py
@@ -44,14 +44,32 @@ def count_words(filename):
         with open(filename) as f:
             contents = f.read()
     except FileNotFoundError:
-        msg = "Sorry, the file " + filename + " does not exist."
-        print(msg)
+        # msg = "Sorry, the file " + filename + " does not exist."
+        # print(msg)
+        pass # Do nonthing when file not found
     else:
         # Calculate numbers of words
         words = contents.split()
         num_words = len(words)
         print("The file " + filename + " has about " + str(num_words) + " words.")
 
-filename = 'programming.txt'
+filename = './oop/programming.txt'
 count_words(filename)
 
+filenames = ['alice.txt', 'siddhartha.txt', 'pi_digits.txt', 'programming.txt']
+for filename in filenames:
+    count_words('./oop/' + filename)
+
+while True:
+    print('Enter two numbers to add: (enter q to quit.)')
+    first_num = input('The first num: ')
+    if first_num == 'q':
+        break
+    second_num = input('The second nm: ')
+    try:
+        sum = int(first_num) + int(second_num)
+        print('The sum of the two numbers are:', str(sum))
+    except TypeError:
+        print('Please enter numbers.')
+    except ValueError:
+        print('Please enter numbers!')
