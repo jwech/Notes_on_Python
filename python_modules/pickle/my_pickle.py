05528dcfb8a80fc1feb6d7ee6dd9cd229dab0bb8
jwech <joewei0208@gmail.com>
2019-09-19 15:43:51 +0800

    edit my_pickle.py

--- python_modules/pickle/my_pickle.py
+++ python_modules/pickle/my_pickle.py
@@ -1,1 +1,23 @@
+# pickle - Object Serialization
+
 # Encode and Decode Data in Strings
+import pickle
+
+data = [{'a': 'A', 'b': 2, 'c': 3.0}]
+print('DATA:', end=' ')
+print(data)
+
+# pickle.dumps()
+data_string = pickle.dumps(data)
+print('PICKLE:: {!r}'.format(data_string))
+
+# upickle the data
+print('BEFORE: ', end=' ')
+print(data)
+data_string = pickle.dumps(data)
+data_loads = pickle.loads(data_string)
+print('AFTER:', end=' ')
+print(data_loads)
+
+print('SAME? :', (data is data_loads))
+print('QUAL? :', (data == data_loads))
